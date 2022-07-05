2241f906187bf709de8cfbb4fe1ad829347c875c
M-Naghavi-2022 <Naghavi.m.business@gmail.com>
2022-07-06 03:30:11 +0430

    MyProfileSerializer updated

--- user/serializers.py
+++ user/serializers.py
@@ -21,15 +21,29 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 class MyProfileSerializer(serializers.ModelSerializer):
 
+    posts_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
+    followings_count = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ["username", "private_status", "first_name", "last_name",
-            "email","phone_number","birthday", "gender", "biography"]
+            "email","phone_number","birthday", "gender", "biography",
+            "posts_count", "followers_count", "followings_count"]
         extra_kwargs = {
             "username": {
                 "read_only": True
             }
         }
+
+    def get_posts_count(self, obj):
+        return obj.posts.count()
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_followings_count(self, obj):
+        return obj.followings.count()
 
 
 class FollowerSerializer(serializers.ModelSerializer):
